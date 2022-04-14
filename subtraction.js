8c420d3125b2fdcbf7c6fbfff90ba6c14c989071
Rodrigo Farinango <rodrigofarinango@Rodrigos-Mac-mini.local>
2022-04-14 14:43:22 -0500

    add test

--- subtraction.js
+++ subtraction.js
@@ -2,3 +2,9 @@ function subtraction(a, b) {
     return a - b;
  }
  module.exports = subtraction;
+
+ function add(a,b){
+     return a +b;
+ }
+
+module.exports = add;
--- subtraction.test.js
+++ subtraction.test.js
@@ -2,3 +2,8 @@ const subtraction = require('./subtraction');
 test('subtracts 4 - 2 to equal 2', () => {
    expect(subtraction(4, 2)).toBe(2);
 });
+
+const add = require('./subtraction');
+test('add 4 + 3 to equal 6', () => {
+    expect(add(4,2)).toBe(6);
+})
