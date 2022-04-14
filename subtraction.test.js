e9a7503768dd05d3e4e9ef953531848c47b06955
Rodrigo Farinango <rodrigofarinango@Rodrigos-Mac-mini.local>
2022-04-14 16:23:22 -0500

    another test

--- subtraction.test.js
+++ subtraction.test.js
@@ -3,3 +3,7 @@ test('subtracts 4 - 2 to equal 2', () => {
    expect(subtraction(4, 2)).toBe(2);
 });
 
+const subtraction = require('./subtraction');
+test('subtracts 4 - 2 to equal 2', () => {
+   expect(subtraction(5, 2)).toBe(7);
+});
